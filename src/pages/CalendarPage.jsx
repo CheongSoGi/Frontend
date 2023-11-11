2e53ef03c49a78b59875981667b99ba9ce54cbba
Stronghye <teenback1004@gmail.com>
2023-11-11 09:54:01 +0900

    아침최종

--- src/pages/CalendarPage.jsx
+++ src/pages/CalendarPage.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { useEffect, useState } from "react";
 import MainLayout from "../layouts/MainLayout";
 
@@ -7,6 +6,8 @@ import { DemoContainer, DemoItem } from "@mui/x-date-pickers/internals/demo";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 import { DateCalendar } from "@mui/x-date-pickers/DateCalendar";
+import { Title } from "../components/Title";
+import { faCalendar } from "@fortawesome/free-solid-svg-icons";
 
 export default function CalendarPage() {
     const [value, setValue] = useState(dayjs("2022-04-17"));
@@ -19,21 +20,10 @@ export default function CalendarPage() {
 
     return (
         <MainLayout>
+            <Title icon={faCalendar}>달력 보기</Title>
             <LocalizationProvider dateAdapter={AdapterDayjs}>
                 <DateCalendar value={value} onChange={(newValue) => setValue(newValue)} />
             </LocalizationProvider>
         </MainLayout>
     );
-=======
-import { Title } from "../components/Title";
-import MainLayout from "../layouts/MainLayout";
-import { faCalendar } from "@fortawesome/free-solid-svg-icons";
-
-export default function CalendarPage() {
-  return (
-    <MainLayout>
-      <Title icon={faCalendar}>달력 보기</Title>
-    </MainLayout>
-  );
->>>>>>> 10661df31fb96b2c7e582d4fa2a38ff051fe837c
 }
